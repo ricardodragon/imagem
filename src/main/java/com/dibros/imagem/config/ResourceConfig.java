058900eefe0e88396ee9cfe636c3e2daa4377116
ricardo <ricardoelfuego@gmail.com>
2026-08-09 17:26:17 -0300

    teste

--- src/main/java/com/dibros/imagem/config/ResourceConfig.java
+++ src/main/java/com/dibros/imagem/config/ResourceConfig.java
@@ -11,6 +11,6 @@ public class ResourceConfig implements WebFluxConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry resourceHandlerRegistry){
         resourceHandlerRegistry
             .addResourceHandler("/public/imagens/**")
-            .addResourceLocations("file:///C:/Users/ricar/Desktop/projects/SERVIDOR - PROD -/imagens/");
+            .addResourceLocations("file:///C:/Users/ricar/Desktop/projects/servidor-prod/");
     }
 }
